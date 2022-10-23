2fe5bf23a3a8b0489d078eb48fe283a1c9f29c64
touhidcodes <touhidcodes@gmail.com>
2022-10-23 12:36:33 +0600

    generate pin

--- pin-genarator.js
+++ pin-genarator.js
@@ -1,10 +1,18 @@
+// Common Function
 function getElementByElementId(elementId) {
   document.getElementById(elementId);
 }
 
-document.getElementById("generate-btn").addEventListener("click", function () {
-  const outputPin = document.getElementById("output-pin");
+// Generate Pin Function
+function generatePin(elementId) {
+  const outputPin = document.getElementById(elementId);
   const randomPin = Math.random() * 9000 + 1;
   const randomPinInt = Math.floor(1000 + randomPin);
   outputPin.value = randomPinInt;
+  return randomPin;
+}
+
+// Generate Pin
+document.getElementById("generate-btn").addEventListener("click", function () {
+  generatePin("output-pin");
 });
